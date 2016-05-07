f73c91e2429eea0cccacc8442e2ce540e1c398f1
skatermonk88 <akhoang88@gmail.com>
2016-05-07 12:46:16 +0700

    truncate index pages content to only display the except and not the full article

--- template-parts/content.php
+++ template-parts/content.php
@@ -46,16 +46,12 @@
 
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'anh-popperscores' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'anh-popperscores' ),
-				'after'  => '</div>',
-			) );
+			the_excerpt();
+			// the_content( sprintf(
+			// 	/* translators: %s: Name of current post. */
+			// 	wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'anh-popperscores' ), array( 'span' => array( 'class' => array() ) ) ),
+			// 	the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			// ) );
 		?>
 	</div><!-- .entry-content -->
 
